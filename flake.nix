b78052c5e26c7dbf067df8870038460629650216
Hraban Luyat <hraban@0brg.net>
2023-01-04 18:03:49 +0100

    build: nix flake support

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Hraban's git tools";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            packages.default = import ./default.nix { inherit pkgs; };
+          });
+}
