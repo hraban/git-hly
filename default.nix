581dd8ee3ad996ed705f996c505043c2bada6089
Hraban Luyat <hraban@0brg.net>
2023-01-07 11:47:04 +0100

    build: use flakes to build, default.nix as compat

--- default.nix
+++ default.nix
@@ -1,36 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with rec {
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore.nix";
-    rev = "a20de23b925fd8264fd7fad6454652e142fd7f73";
-    sha256 = "sha256-8DFJjXG8zqoONA1vXtgeKXy68KdJL5UaXR8NtVMUbx8=";
-  };
-  cleanSource = src: (pkgs.callPackage gitignoreSrc {}).gitignoreSource (pkgs.lib.cleanSource src);
-
-  lispPackagesLite = import (pkgs.fetchFromGitHub {
-    owner = "hraban";
-    repo = "cl-nix-lite";
-    rev = "40b368ed386ff3e7739985f6633e0625f8534f2c";
-    sha256 = "sha256-11/0ZTvxrI5PEhzOKUbZj0IAdiMiLpGGSs576LwFZ6w=";
-  }) { inherit pkgs; };
-};
-
-with lispPackagesLite;
-
-lispDerivation {
-  lispSystem = "git-hly";
-  pname = "git-hly";
-  version = "0.0.1";
-  src = cleanSource ./.;
-  dontPatchShebangs = true;
-  dontStrip = true;
-  lispDependencies = [
-    alexandria
-    arrow-macros
-    inferior-shell
-    trivia
-    trivia-ppcre
-  ];
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
--- flake.nix
+++ flake.nix
@@ -1,17 +1,73 @@
+# Copyright © 2022, 2023  Hraban Luyat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 {
   description = "Hraban's git tools";
 
   inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:hercules-ci/gitignore.nix";
+    };
+    cl-nix-lite = {
+      flake = false;
+      url = "github:hraban/cl-nix-lite";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-          {
-            packages.default = import ./default.nix { inherit pkgs; };
-          });
+  outputs = { self, nixpkgs, flake-utils, gitignore, cl-nix-lite, ... }:
+    with rec {
+      ignoredPaths = [
+        ./build.lisp
+        ./build-asdf-only.lisp
+        ./ql-install-deps.lisp
+      ];
+    };
+    flake-utils.lib.eachDefaultSystem (system:
+      with rec {
+        pkgs = nixpkgs.legacyPackages.${system};
+        lispPackagesLite = import cl-nix-lite { inherit pkgs; };
+        cleanSource = src: pkgs.lib.cleanSourceWith {
+          filter = path: type: (! builtins.elem (/. + path) ignoredPaths);
+          src = pkgs.lib.cleanSource (gitignore.lib.gitignoreSource src);
+        };
+      };
+      {
+        packages = {
+          # This is for normal people
+          default = with lispPackagesLite; lispDerivation {
+            lispSystem = "git-hly";
+            pname = "git-hly";
+            version = "0.0.1";
+            src = cleanSource ./.;
+            lispDependencies = [
+              alexandria
+              arrow-macros
+              inferior-shell
+              trivia
+              trivia-ppcre
+            ];
+            dontStrip = true;
+            meta = {
+              license = pkgs.lib.licenses.agpl3Only;
+            };
+          };
+        };
+      });
 }
